4dc8f47e99be77e1104d346bd2380413e529f9f6
letItCurl <rolandlopez.developer@gmail.com>
2024-01-28 06:23:46 +0700

    ⚡️ Refactoring default onboarding.

--- config/initializers/custom/onboarding_resume.rb
+++ config/initializers/custom/onboarding_resume.rb
@@ -1,36 +1,53 @@
+about = <<-EOH
+Ruby on Rails developer with 7 years of hands-on experience, dedicated to advancing the boundaries of technology.
+
+Self-taught, I have exclusively worked with Ruby on Rails teams, contributing to startup and corporate teams of varying sizes.
+Additionally, I've successfully launched SaaS projects.
+
+My expertise is geared towards assisting fellow developers in achieving:
+
+- Building a strong online presence for their tech careers and side projects.
+- Ensuring consistency in marketing and content creation for side projects.
+- Providing personalized coaching on tech marketing and coding.
+- Defining a compelling online tech persona for various platforms.
+If these objectives resonate with you, I'm eager to learn more about your project. Please feel free to schedule a meeting at your convenience.
+
+Find me here: https://linktr.ee/roland_lopez
+EOH
+
 ONBOARDING_RESUME = {
-  title: 'Coach - Ruby On Rails Expert',
-  about: "Self-taught developer who fell in love with the Ruby on Rails framework 8 years ago.\n\nSince then, I've been exclusively working with Rails in startup and corporate teams of all sizes,\nwhile also launching SaaS projects to the market.\n\nI'm focusing on helping developers that need:\n        \n- Building momentum in your socials for your tech career and side-projects.\n- Consistency in marketing and content creation for side-projects.\n- Personal coaching about tech marketing and coding.\n- Defining your online tech persona for your platforms.\n\nIf this resonates with you, I would love to know more about your project. \n\nPlease feel free to book a meeting at your convenience. \n\n Find me here: https://linktr.ee/roland_lopez",
-  developer_since: Date.new(2018, 2, 3),
+  title: "Ruby on Rails Specialist",
+  about: about,
+  developer_since: Date.new(2017, 1, 20),
   location: 'Thailand, Bangkok',
   experiences_attributes: [
     {
-      title: 'Youtube Channel: Rad Roland',
-      description: "Tech channel created by a biker: Code to chase dreams.\nStrive to show that you can have a successful tech career and still follow your passion with intensity.",
+      title: 'APPLICATION SOFTWARE ENGINEER - Mars Mission Team',
+      description: "In-depth technical content creator on the 'Rad Roland' YouTube channel, merging the world of coding with a passion for motorsports. Striving to inspire by demonstrating the possibility of a successful tech career alongside intense personal pursuits.",
       skills: 'Ruby On Rails, Motorsports, Marketing, Content Creation, Scripting, SEO, Film Making, Growth Hacking',
-      started_at: Time.current,
-      ended_at: Time.current - 5.years
+      started_at: Time.current - 5.years,
+      ended_at: Time.current
     },
     {
-      title: 'Freelance Face - Founder',
-      description: "Bootstrapped SaaS project that aims to be the face of the freelance developer who needs to reach out to recruiters.\nFreelance Face provides a tailored resume using GenAI with video and calendar links for each job description while tracking views of your resume and sending you an email when the resume is viewed by a recruiter.",
+      title: 'Founder/Lead Developer - Freelance Face',
+      description: "Initiated and bootstrapped 'Freelance Face,' a SaaS project designed to be the face of freelance developers reaching out to recruiters. Implemented GenAI for personalized resumes with video and calendar links per job description.",
       skills: 'Ruby On Rails, Hotwire, Tailwind, Postgresql, Hatchbox.io, OpenAI',
-      started_at: Time.current - 6.month,
-      ended_at: Time.current - 12.month
-    },
-    {
-      title: 'Founder/Coach @ Nomad Joker',
-      description: 'Online Mentoring Course limited to 30 people to help developers reach their childhood dreams using coding and find themselves in marketing. Find more info here: https://linktr.ee/roland_lopez',
-      skills: 'Ruby On Rails, Hotwire, Personal Coaching',
       started_at: Time.current - 12.month,
       ended_at: Time.current - 18.month
     },
     {
-      title: 'Email Summarizer - Founder',
-      description: "Bootstrapped SaaS project: A Google extension that writes emails for you.\nThis project was launched on AppSumo and had 150 users and summarized more than 27000 emails!",
-      skills: 'Ruby On Rails, Hotwire, Google Extension, Stripe, Coaching, Svelte',
+      title: 'Founder/Coach - Nomad Joker Mentoring Course',
+      description: '"Nomad Joker" is an online mentoring course, limited to 30 participants, guiding developers in realizing childhood dreams through coding and self-discovery in marketing. For more details, visit: https://linktr.ee/roland_lopez',
+      skills: 'Ruby On Rails, Hotwire, Personal Coaching',
       started_at: Time.current - 18.month,
       ended_at: Time.current - 24.month
+    },
+    {
+      title: 'Founder/Lead Developer - Email Summarizer',
+      description: "Launched and managed the 'Email Summarizer' SaaS project, a Google extension automating email composition. Successfully introduced the project on AppSumo, garnering 150 users and summarizing over 27,000 emails.",
+      skills: 'Ruby On Rails, Hotwire, Google Extension, Stripe, Coaching, Svelte',
+      started_at: Time.current - 24.month,
+      ended_at: Time.current
     }
   ],
   meeting_url: 'https://calendly.com/rolandlopez/tech-nomad-screening',
--- config/initializers/custom/onboarding_resume_replica.rb
+++ config/initializers/custom/onboarding_resume_replica.rb
@@ -1,6 +1,39 @@
+job_description = <<-EOH
+APPLICATION SOFTWARE ENGINEER - MARS MISSION TEAM
+
+Location: Cape Canaveral, FL
+
+SpaceX, a pioneering space exploration company with a vision to make humanity multi-planetary, is seeking an Application Software Engineer to join our Mars Mission Team. At SpaceX, we believe in a future where humans explore the stars, and our mission is to actively develop the technologies that will enable human life on Mars. As a part of the application software team, you will play a crucial role in creating mission-critical applications exclusively using Ruby on Rails technology.
+
+RESPONSIBILITIES:
+
+Develop and maintain highly reliable software solutions exclusively using Ruby on Rails, tailored for the Mars Mission.
+Create new applications that streamline and enhance the operation of SpaceX's Mars-bound vehicles.
+Build prototypes to validate key design concepts and identify technical constraints within the Ruby on Rails framework.
+Take ownership of all aspects of software engineering and product development for the Mars Mission applications.
+Deeply analyze the unique challenges of Mars missions, providing efficient and innovative solutions through Ruby on Rails.
+Collaborate with a diverse team of engineers to design and produce solutions that align with the mission objectives at an accelerated pace.
+Utilize Hatchbox as a Platform-as-a-Service (PaaS) to host and deploy Ruby on Rails applications for the Mars Mission.
+BASIC QUALIFICATIONS:
+
+Bachelor's degree in computer science, engineering, math, or a scientific discipline; OR 2+ years of professional experience in software development.
+Experience in full-stack development with a strong emphasis on Ruby on Rails.
+PREFERRED SKILLS AND EXPERIENCE:
+
+Proven programming experience in Ruby on Rails, with expertise in building robust, scalable web applications.
+Familiarity with version control, continuous integration, and continuous delivery concepts within the Ruby on Rails ecosystem.
+Strong understanding of software system design, profiling, and application performance improvement using Ruby on Rails.
+Experience with build systems and package management tools relevant to Ruby on Rails development.
+Proficiency in UNIX-like operating systems and tools, including Terraform, Ansible, Puppet, specifically tailored for Ruby on Rails applications.
+Familiarity with in-stream data processing using technologies such as Apache Kafka, RabbitMQ, or ActiveMQ, integrated seamlessly with Ruby on Rails.
+Production experience with Docker and Kubernetes for deploying Ruby on Rails applications.
+Front-end experience in Ruby on Rails-compatible frameworks or libraries.
+Database experience with PostgreSQL, SQL Server, or similar, specifically adapted for Ruby on Rails applications.
+EOH
+
 ONBOARDING_RESUME_REPLICA = {
   company_name: 'Space X',
   recruter_full_name: 'Elon Must',
   recruter_email: 'elon@spacex.com',
-  job_description: "As the Senior Tech Lead at Loupe, Inc., you will play a crucial role in the development and maintenance of Loupe's suite of visual art streaming solutions, including our cloud-based SaaS solution, TV applications and DAM. Reporting directly to the Leadership team, you will be responsible for leading a team of talented engineers while directly contributing in all phases of the development lifecycle, from requirement gathering and design, to solution deployment and support. \n\nThis is a full-time remote position Loupe is made up of a small distributed team across the US (strong preference for candidates residing in Georgia, California, New York, New Jersey, or Florida).\n\nRESPONSIBILITIES\nDefining and building new architectures around product needs\nTroubleshooting and debuging complex technical issues, providing timely and effective resolutions.\nWriting clean, well-structured, scalable, and efficient code\nCoaching and mentoring team members through pair programming, code reviews, and process documentation\nWorking closely with the Product Team and Leadership to gather requirements and translate them into technical specifications and roadmaps that achieve company objectives and long term visions.\nDeveloping and maintaining coding standards to ensure high quality of code readability and reliability\nQUALIFICATIONS\nProven work experience in a similar role\nAbility to lead and coach engineers and help them grow\nAbility to architect complete fullstack features and solutions\nStrong expertise building on a Ruby on Rails tech stack\nExcellent work ethic and meticulous attention to detail, ability to work independently and multitask while staying organized  in a fast-paced and dynamic environment.\nStrong emotional intelligence and communication skills, working proactively and collaboratively with a distributed team\nAppreciation of Lifestyle/Culture/Music/Art/Tech \nNice to have: \nExperience building media platforms\nPrior experience at a media company\nExperience building tvOS, mobile, or other connected devices applications \nBasic understanding of content management systems or digital asset management systems"
+  job_description: job_description
 }.freeze
