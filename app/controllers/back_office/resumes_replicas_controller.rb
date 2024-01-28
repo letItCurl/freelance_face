b66bb0648b8982c2d2b16807688633ebc8e06baf
letItCurl <rolandlopez.developer@gmail.com>
2024-01-28 09:38:01 +0700

    ☕️ Email working.

--- app/controllers/back_office/resumes_replicas_controller.rb
+++ app/controllers/back_office/resumes_replicas_controller.rb
@@ -34,7 +34,7 @@ module BackOffice
 
       respond_to do |format|
         if @resumes_replica.valid?
-          ResumesReplicas::GenerateJob.perform_async(@resumes_replica.id)
+          ResumesReplicas::GenerateJob.perform_async(@resumes_replica.id) if @resumes_replica.not_started?
           format.html { redirect_to resumes_replicas_path, notice: 'Resume was successfully created.' }
         else
           format.turbo_stream { render turbo_stream: turbo_stream.update(:model_errors, partial: 'layouts/shared/model_errors', locals: { model: @resumes_replica }) }
--- app/controllers/front_office/resumes_controller.rb
+++ app/controllers/front_office/resumes_controller.rb
@@ -5,23 +5,8 @@ class FrontOffice::ResumesController < FrontOfficeController
     @resume = Resume.includes(:user).where(users: {username: params[:username]}, resumes: {id: params[:resume_id]})&.first
     raise AbstractController::ActionNotFound.new("not found") if @resume.nil?
     @user = @resume.user
-    puts "☕️"
-    puts "REMOTE IP#{request.remote_ip}"
-    puts "UA #{request.user_agent}"
-    puts "User Device LIST 🃏"
-    puts @user.devices.inspect
-    puts @user.devices.class
-    puts "COUNT"
-    puts @user.devices.where(remote_ip: request.remote_ip, user_agent: request.user_agent).count
-    puts "User Device 🃏"
-    puts @user.devices.where(remote_ip: request.remote_ip, user_agent: request.user_agent).nil?
-    puts "❤️❤️❤️❤️❤️"
-    puts @user.devices.where(remote_ip: request.remote_ip, user_agent: request.user_agent)&.first&.nil?
-    puts "❤️❤️❤️❤️❤️"
-    puts @user.devices.where(remote_ip: request.remote_ip, user_agent: request.user_agent)&.first
-    puts "❤️❤️❤️❤️❤️"
+
     if @user.devices.where(remote_ip: request.remote_ip, user_agent: request.user_agent).count == 0
-      puts "IN LOOP"
       @resume.views.create(remote_ip: request.remote_ip, user_agent: request.user_agent)
     end
   end
